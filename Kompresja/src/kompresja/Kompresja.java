6c63c35061183ff80ca914b9c7ab908e55af8921
adaol322 <adam.olechno1@gmail.com>
2018-10-17 21:32:41 +0200

    i/o test in java

---
+++ Kompresja/src/kompresja/Kompresja.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kompresja;
+import java.util.Scanner; 
+import java.io.Console;
+
+/**
+ *
+ * @author Kappa123
+ */
+public class Kompresja {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        System.out.println(s);
+        
+        
+    }
+    
+}
